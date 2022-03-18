714cf6833df408c73de6bcefd7b617762700a8d0
thelumiereguy <46375353+thelumiereguy@users.noreply.github.com>
2022-03-18 18:05:34 +0530

    added dialog

---
+++ src/main/java/com/thelumiereguy/compose_helper/intention/presentation/dialog/ReleaseNoteDialog.kt
@@ -0,0 +1,24 @@
+package com.thelumiereguy.compose_helper.intention.presentation.dialog
+
+import com.intellij.openapi.ui.DialogWrapper
+import javax.swing.Action
+import javax.swing.JTextArea
+import javax.swing.JPanel
+import javax.swing.JComponent
+
+class ReleaseNoteDialog : DialogWrapper(), Action {
+    private val txtReleaseNote: JTextArea? = null
+    private val panelWrapper: JPanel? = null
+    override fun createCenterPanel(): JComponent? {
+        return panelWrapper
+    }
+
+    override fun getOKAction(): Action {
+        return this
+    }
+
+    override fun actionPerformed(e: ActionEvent?) {
+        // save value to project state
+        super.doOKAction()
+    }
+}
